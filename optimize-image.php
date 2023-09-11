b6e7a62d2a5f763b823542a65b8dc41a58ec6ed6
qianzhiwei <380659419@qq.com>
2023-09-11 15:21:01 +0800

    feature: using webp

--- optimize-image.php
+++ optimize-image.php
@@ -1,7 +1,7 @@
 <?php
 #/static/images/focus01_img.jpg
 #https://www.ilmsmile.com/static/images/focus01_img.jpg
-//http://localhost:8008/c/static/images/focus01_img.jpg
+//http://localhost:8008/static/images/focus01_img.jpg.webp
 $wgetBin = '/usr/local/bin/wget';
 $convertBin = '/usr/local/bin/convert';
 if(!file_exists($wgetBin)) die('wget no exists!');
@@ -9,6 +9,7 @@ $localImageRoot = dirname(__FILE__) ;
 
 $dstBaseUrl = 'https://www.ilmsmile.com';
 $uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+$uri = preg_replace('/(\.webp)$/', '', $uri);
 $ext = pathinfo($uri, PATHINFO_EXTENSION);
 //$newFile = md5($realUri) . '.' . $ext;
 $newFile = pathinfo($uri, PATHINFO_FILENAME);
@@ -18,8 +19,9 @@ $subPath = preg_replace('/(\/[\w_]+\.\w+)$/', '', $uri);
 $newPath = $localImageRoot . $subPath;
 if(!file_exists($newPath)) mkdir($newPath, 0755, true);
 
-$newFilePath = $newPath . '/' . $newFile . '.' . $ext;
+$newFilePath = $newPath . '/' . $newFile . '.' . $ext . '.webp';
 $dstfileUrl = $dstBaseUrl . $uri;
+
 if(!file_exists($newFilePath)){
     $cmd = sprintf('%s -q "%s" -O %s', $wgetBin, $dstfileUrl, $newFilePath);
     system($cmd);
